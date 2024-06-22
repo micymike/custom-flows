5ae7c8145bbe878403be4d69a3b9e0b2c2c86d8d
micymike <mosesmichael878@gmail.com>
2024-06-22 20:52:32 +0300

    "Mark custom-flows as dirty"

--- api_call_example/my_actions/reverse_string.py
+++ api_call_example/my_actions/reverse_string.py
@@ -2,28 +2,27 @@ from asyncflows import Action, BaseModel, Field
 
 import aiohttp
 
-
 class Inputs(BaseModel):
     input_string: str
-
+    word_index: int = 0
 
 class Outputs(BaseModel):
     reversed_string: str
-
 
 class MyReverseString(Action[Inputs, Outputs]):
     name = 'my_reverse_string'
 
     async def run(self, inputs: Inputs) -> Outputs:
-        return Outputs(
-            reversed_string=inputs.input_string[::-1]
-        )
+        words = inputs.input_string.split()
+        if 0 <= inputs.word_index < len(words):
+            words[inputs.word_index] = words[inputs.word_index][::-1]
+        reversed_string = ' '.join(words)
+        return Outputs(reversed_string=reversed_string)
 
 ### from test.py
 
 import os
 from asyncflows import AsyncFlows
-
 
 async def main():
     dirname = os.path.dirname(__file__)
@@ -34,9 +33,9 @@ async def main():
     # Run the flow and return the default output (result of the extract_title action)
     result = await flow.set_vars(
         my_input="hahahah hihihih",
+        word_index=1  # Reverse the second word
     ).run()
     print(result)
-
 
 if __name__ == "__main__":
     import asyncio
